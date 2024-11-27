4f1f5481061d6daf75f3b49a5b4bd38ca2865f2e
icelal-kskn <programmer.keskincelal@gmail.com>
2024-11-27 19:18:00 +0300

    Header finished

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+
+    <div class="background-container">
+      <img src="./assets/bg-image.jpeg" alt="Background Image">
+      <div class="header-overlay">
+        <AppHeader/>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.background-container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.background-container img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  opacity: 2.85;
+  z-index: 1;
+  filter: brightness(0.7);
+}
+
+.header-overlay {
+  position: relative;
+  z-index: 2;
+  width: 100%;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,109 @@
+<template>
+  <header>
+    <nav>
+      <div class="logo">
+        <img alt="Home Real Estate" class="logo-image" src="../assets/logo.png" >
+
+      </div>
+      <div class="nav-menu">
+          <a href="#" class="sign-in">Sign In</a>
+          <a href="#" class="register">Register</a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style scoped>
+header {
+  padding: 1rem 0;
+  z-index: 100;
+  opacity: 0.9;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 1rem;
+}
+
+.logo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+.logo-image {
+  height: 5rem;
+  opacity: 3;
+  filter: brightness(2.5);
+}
+
+.nav-menu {
+  display: flex;
+  gap: 1rem;
+}
+
+.nav-menu a {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  text-decoration: none;
+  justify-content: center;
+  cursor: pointer;
+  width: 6.5rem ;
+  height: 2.5rem;
+  font-size: 1rem;
+  font-weight: 500;
+  transition: color 0.3s ease;
+  border: solid #757575;
+  border-width: 0.1rem;
+  border-radius: 8px;
+}
+
+
+
+
+.sign-in{
+  background-color: #FFFFFFCC;
+  color: #2C2C2C;
+}
+
+.sign-in:hover {
+  color: rgba(117, 115, 115, 0.902);
+}
+
+.register{
+  background-color: #2C2C2C;
+  color: #fff;
+}
+
+.register:hover {
+  color: #e2e8f0;
+}
+
+
+@media (max-width: 768px) {
+  nav {
+    flex-direction: column;
+  }
+
+  .nav-menu {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .nav-menu div {
+    margin: 0.5rem 0;
+  }
+}
+</style>
